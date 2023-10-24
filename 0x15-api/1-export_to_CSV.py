5394dc2fe80b3a7fe8afda96b6bba6425e0ecf79
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-24 15:42:29 +0300

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-""" script to export data in the CSV format."""
+"""Script to export data in the CSV format with double quotation marks."""
 
 if __name__ == "__main__":
     import csv
-    from requests import request
+    from requests import get
     import sys
 
     user_id = sys.argv[1]
     task_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     user_info_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-    filename = "{}.csv".format(user_id)  # to store the csv data
-    user_tasks_list = []  # 2d list to store all tasks of a given user
+    filename = f"{user_id}.csv"  # To store the CSV data
 
-    tasks_response = (request("GET", task_url)).json()
-    user_response = request("GET", user_info_url).json()
+    tasks_response = get(task_url).json()
+    user_response = get(user_info_url).json()
 
     employee_name = user_response.get('username')
 
-    # add each task, as an array, to our 2d list above
-    for task in tasks_response:
-        user_tasks_list.append(
-            [user_id, employee_name, task.get('completed'), task.get('title')]
-            )
-
-    # write data to the csv file
     with open(filename, 'w', newline='') as csvfile:
-        data = csv.writer(csvfile)
-        data.writerows(user_tasks_list)
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+        """
+        Write the header
+        csv_writer.writerow(["userId", "username", "completed", "title"])"""
+
+        for task in tasks_response:
+            csv_writer.writerow([
+                user_id,
+                employee_name,
+                task.get("completed"),
+                task.get("title")])
+    print(f"Data for user {user_id} exported to {filename}")
