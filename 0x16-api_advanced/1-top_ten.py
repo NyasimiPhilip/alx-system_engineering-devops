195f5ff2d071a5617d294e74b23ecb2135005e83
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-11-07 20:48:55 +0300

    Update

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Function to print hot posts on a given Reddit subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+     """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            children = data.get("data", {}).get("children", [])
+            for child in children:
+                title = child.get("data", {}).get("title", "")
+                if title:
+                    print(title)
+        elif response.status_code == 404:
+            return
+    except requests.RequestException:
+        return
+
+if __name__ == "__main__":
+    subreddit = "python"  # Replace with the desired subreddit
+    top_ten(subreddit)
