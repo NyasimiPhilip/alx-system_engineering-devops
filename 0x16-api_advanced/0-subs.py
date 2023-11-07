ef954490d02e242f870c596ec8e7228d630c8e71
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-11-07 20:06:38 +0300

    Update

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data["data"]["subscribers"]
+            return subscribers
+        elif response.status_code == 404:
+            return 0
+        else:
+            return None
+
+    except requests.RequestException:
+        return None
