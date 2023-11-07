3375cb66d31687885468c6ff6721b41b35af675b
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-11-07 20:36:59 +0300

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
