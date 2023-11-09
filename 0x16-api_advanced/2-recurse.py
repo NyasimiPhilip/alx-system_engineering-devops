69fc75292efee6c691e5f0776e37d7f3a26b3f04
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-11-09 09:29:48 +0300

    Update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,25 +4,39 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Retrieve and gather titles of trending posts from a specified subreddit."""
+def recurse(subreddit, hot_list=None, after="", count=0):
+    """Retrieve and gather titles oft
+    trending posts from a specified subreddit."""
+    if hot_list is None:
+        hot_list = []
+
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     headers = {
-        "User-Agent": "custom-reddit-trending-fetcher:v1.0.0 (by /u/your_username)"
+        "User-Agent": (
+            "custom-reddit-trending-fetcher:v1.0.0 "
+            "(by /u/your_username)"
+        )
     }
     params = {
         "after": after,
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+
+    response = requests.get(
+        url,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+
     if response.status_code == 404:
         return None
 
     data = response.json().get("data")
     after = data.get("after")
     count += data.get("dist")
+
     for item in data.get("children"):
         hot_list.append(item.get("data").get("title"))
 
