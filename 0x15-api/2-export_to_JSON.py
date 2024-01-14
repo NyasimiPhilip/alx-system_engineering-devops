c7756cd4bc439c611873ddac20ccdf9404731061
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2024-01-14 14:30:40 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,36 +1,46 @@
 #!/usr/bin/python3
 """Script to export data in JSON format."""
 
+# Check if the script is being run as the main program
 if __name__ == "__main__":
     import json
     from requests import get
     import sys
 
+    # Get the user ID from the command line arguments
     user_id = sys.argv[1]
+
+    # Define the URLs for task information and user information
     task_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
     user_info_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
 
-    filename = f"{user_id}.json"  # To store the JSON data
+    # Define the filename to store the JSON data
+    filename = f"{user_id}.json"
 
+    # Make API requests to retrieve task and user information
     tasks_response = get(task_url).json()
     user_response = get(user_info_url).json()
 
+    # Extract the username from the user information
     employee_name = user_response.get('username')
 
+    # Initialize an empty list to store task details for the user
     user_tasks_list = []
+
+    # Loop through each task and append a dictionary with task details to the list
     for task in tasks_response:
-        # For each task of a user, append a dictionary
-        # with the task's details into the above array
         user_tasks_list.append({
             "task": task.get('title'),
             "completed": task.get('completed'),
             "username": employee_name
         })
 
+    # Open the JSON file for writing
     with open(filename, 'w') as jsonFile:
-        # Write the data where the key is the user ID and
-        # the value is an array of dictionaries for each task
+        # Create a dictionary where the key is the user ID
+        # and the value is an array of dictionaries for each task
         user_json_data = {
             user_id: user_tasks_list
         }
+        # Write the JSON data to the file with indentation for readability
         jsonFile.write(json.dumps(user_json_data, indent=4))
